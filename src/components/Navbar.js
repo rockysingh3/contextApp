3a116893289fc2a6d5da68aceea90b2f666ddfd9
Rocky Singh <rockygsingh@icloud.com>
2020-08-24 01:01:55 -0400

    used context consumer instead of static context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,29 @@
-import React, { Component } from 'react';
-import { ThemeContext } from '../contexts/ThemeContext';
+import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 class Navbar extends Component {
-
-    static contextType = ThemeContext
-
-    render() { 
-        // destructure the state from context 
-        const { isLightTheme, light, dark } = this.context
-        const theme = isLightTheme ? light : dark
-        return (  
-            
-            // using the globle state 
+  render() {
+    return (
+      <ThemeContext.Consumer>
+        {(context) => {
+          // destructure the state from context
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            // using the globle state
             <nav style={{ background: theme.ui, color: theme.syntax }}>
-                <h1>Context App</h1>
-                <ul>
-                    <li className="container">Home</li>
-                    <li>About</li>
-                    <li>Content</li>
-                </ul>
+              <h1>Context App</h1>
+              <ul>
+                <li className="container">Home</li>
+                <li>About</li>
+                <li>Content</li>
+              </ul>
             </nav>
-        );
-    }
+          );
+        }}
+      </ThemeContext.Consumer>
+    );
+  }
 }
- 
-export default Navbar
+
+export default Navbar;
